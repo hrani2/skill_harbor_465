0a9f666700af4a7c82106afa689cdb173bdcc123
mirrakannappan123 <mirrak2@illinois.edu>
2023-11-15 16:56:28 -0600

    worked on join. Still need to figure out join code, learn more, and filter

--- skill_harbor/Search.js
+++ skill_harbor/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Image } from 'react-native'
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, StatusBar, SafeAreaView , ScrollView, Modal} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
@@ -14,31 +14,26 @@ const MyModal = ({modalVisible, setModalVisible, navigation}) => (
       }}>
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
+
           <View style={styles.iconview}>
-          <TouchableOpacity style = {styles.helpicon}>
-            <Icon name="question-circle" size={25}  color="#00507B"/> 
+          <TouchableOpacity>
+            <Text style={styles.reseticon}>Reset</Text> 
            </TouchableOpacity>
+          <Text style={styles.modalTitle}>FILTERS</Text>
             <TouchableOpacity style = {styles.closeicon}
             onPress={() => setModalVisible(false)}>
             <Icon name="times" size={25} color="#00507B"/> 
            </TouchableOpacity>
           </View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() =>  {navigation.navigate('Search'); setModalVisible(false);}}>
-            <Text style={styles.textStyle}>Search All Teams</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {/* Your action for second option */}}>
-            <Text style={styles.textStyle}>Join Organization</Text>
-          </TouchableOpacity>
-        </View>
+          <View style={styles.lineone} />
+
+         </View>
       </View>
     </Modal>
   );
 
 const Search = ( {navigation}) => {
+  const [modalVisible, setModalVisible] = useState(false);
     return (
         <View style={styles.container}>
              <ScrollView>
@@ -53,16 +48,107 @@ const Search = ( {navigation}) => {
           </View>
         </View>
         
-        <TouchableOpacity style={styles.filterButton}>
+        <TouchableOpacity style={styles.filterButton} onPress={() => setModalVisible(true)}>
             <Text style={styles.filterText}>Filters</Text>
             <View style={styles.filterIcon}>
                 <Icon name="sliders" size={20} color="#FFF" />
             </View>
+            <MyModal 
+              modalVisible={modalVisible}
+              setModalVisible={setModalVisible}
+          />
           </TouchableOpacity>
+
+            <View style={styles.cardContainer}>
+
+              <View style={styles.card}>
+                <View style={styles.rowformat}>
+                  <Image
+                    source={{ uri: 'https://via.placeholder.com/50' }} // Replace 
+                    style={styles.profilePic}
+                  />
+                <Text style={styles.cardTitle}>Font-astic Six</Text>
+                </View>
+                <Text style={styles.summary}> We are a dynamic group dedicated 
+                to developing a groundbreaking team-forming app, designed to transform the 
+                way teams are created and managed. With a focus on leveraging advanced algorithms 
+                and user-friendly interfaces, this app seeks to facilitate the team formation process 
+                by intelligently matching individuals based on their skills, experience, and 
+                interpersonal compatibility. Currently, Font-astic Six is on the lookout for
+                enthusiastic members who possess expertise in UI/UX design. </Text>
+              <TouchableOpacity style={styles.learnMoreButton}>
+                <Text style={styles.learnMoreText}>Learn More</Text>
+              </TouchableOpacity>
+              </View>
+
+              {/* Card 2 */}
+              <View style={styles.card}>
+                <View style={styles.rowformat}>
+                  <Image
+                    source={{ uri: 'https://via.placeholder.com/50' }} // Replace 
+                    style={styles.profilePic}
+                  />
+                  <Text style={styles.cardTitle}>UI Utopia</Text>
+              </View>
+              <Text style={styles.summary}> We are a dynamic group dedicated 
+                to developing a groundbreaking team-forming app, designed to transform the 
+                way teams are created and managed. With a focus on leveraging advanced algorithms 
+                and user-friendly interfaces, this app seeks to facilitate the team formation process 
+                by intelligently matching individuals based on their skills, experience, and 
+                interpersonal compatibility. Currently, Font-astic Six is on the lookout for
+                enthusiastic members who possess expertise in UI/UX design. </Text>
+              <TouchableOpacity style={styles.learnMoreButton}>
+                <Text style={styles.learnMoreText}>Learn More</Text>
+              </TouchableOpacity>
+              </View>
+
+              {/* Card 3 */}
+              <View style={styles.card}>
+                <View style={styles.rowformat}>
+                  <Image
+                    source={{ uri: 'https://via.placeholder.com/50' }} // Replace 
+                    style={styles.profilePic}
+                  />
+                <Text style={styles.cardTitle}>Vision</Text>
+                </View>
+                <Text style={styles.summary}> We are a dynamic group dedicated 
+                to developing a groundbreaking team-forming app, designed to transform the 
+                way teams are created and managed. With a focus on leveraging advanced algorithms 
+                and user-friendly interfaces, this app seeks to facilitate the team formation process 
+                by intelligently matching individuals based on their skills, experience, and 
+                interpersonal compatibility. Currently, Font-astic Six is on the lookout for
+                enthusiastic members who possess expertise in UI/UX design. </Text>
+              <TouchableOpacity style={styles.learnMoreButton}>
+                <Text style={styles.learnMoreText}>Learn More</Text>
+              </TouchableOpacity>
+              </View>
+
+              {/* Card 4 */}
+              <View style={styles.card}>
+                <View style={styles.rowformat}>
+                  <Image
+                    source={{ uri: 'https://via.placeholder.com/50' }} // Replace 
+                    style={styles.profilePic}
+                  />
+                <Text style={styles.cardTitle}>0(1)</Text>
+                </View>
+                <Text style={styles.summary}> We are a dynamic group dedicated 
+                to developing a groundbreaking team-forming app, designed to transform the 
+                way teams are created and managed. With a focus on leveraging advanced algorithms 
+                and user-friendly interfaces, this app seeks to facilitate the team formation process 
+                by intelligently matching individuals based on their skills, experience, and 
+                interpersonal compatibility. Currently, Font-astic Six is on the lookout for
+                enthusiastic members who possess expertise in UI/UX design. </Text>
+              <TouchableOpacity style={styles.learnMoreButton}>
+                <Text style={styles.learnMoreText}>Learn More</Text>
+              </TouchableOpacity>
+              </View>
+
+            </View>
           </ScrollView>
           <TouchableOpacity style={styles.floatButton} onPress={() => navigation.navigate('Home')}>
           <View style = {styles.homeicon}>
-            <Icon name="home" size={40} color="#FFF" />
+            <Icon name="home" size={40} color="#00507B" />
           </View>
           </TouchableOpacity>
         </View>
@@ -70,6 +156,56 @@ const Search = ( {navigation}) => {
   };
   
   const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalView: {
+      backgroundColor: "white",
+      borderRadius: 20,
+      width: "90%",
+      height: 170,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 9,
+      elevation: 5,
+      alignSelf: 'center',
+    },
+    iconview: {
+      flexDirection: 'row',
+    },
+    modalTitle: {
+      fontSize: 21,
+      color: '#00507B',
+      fontFamily: 'RobotoSlab-Bold',
+      letterSpacing: 1,
+      top: 10,
+    },
+    reseticon: {
+      right: 90,
+      top: 16,
+      color: "red",
+      fontSize:15,
+      alignSelf: 'center',
+    },
+    closeicon: {
+      top: 10,
+      left: 90,
+      paddingBottom: -50,
+    },
+    lineone: {
+      borderBottomWidth: 1,
+      borderColor: '#808080',
+      width: '100%', 
+      top: 12,
+    },
     container: {
       flex: 1,
     },
@@ -126,13 +262,65 @@ const Search = ( {navigation}) => {
         right: 320,
         top: 750,
     },
-    homeicon: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        backgroundColor: "#00507B",
-        alignItems: 'center',
-        justifyContent: 'center',
+    cardContainer: {
+      padding: 10,
     },
+    card: {
+      backgroundColor: '#00507B',
+      borderRadius: 10,
+      padding: 20,
+      marginBottom: 10,
+    },
+    rowformat: {
+      flexDirection: 'row',
+    },
+    profilePic: {
+      width: 50, // Adjust the size as needed
+      height: 50, // Adjust the size as needed
+      borderRadius: 25, // Circular profile pictures
+      marginRight: 10, // Add some spacing between the picture and the title
+      alignSelf: 'flex-start',
+    },
+    cardTitle: {
+      fontSize: 20,
+      fontFamily: 'RobotoSlab-Bold',
+      marginBottom: 10,
+      top: 10,
+      color: '#FFF',
+      letterSpacing: 1,
+    },
+    summary: {
+      fontSize: 16,
+      fontFamily: 'RobotoSlab-Regular',
+      marginBottom: 30,
+      top: 20,
+      color: '#FFF',
+      alignSelf: 'center',
+    },
+    learnMoreButton: {
+      backgroundColor: '#FFF',
+      padding: 10,
+      borderRadius: 7,
+      alignSelf: 'center',
+    },
+    learnMoreText: {
+      color: '#00507B',
+      fontSize: 16,
+      fontFamily: 'RobotoSlab-Regular',
+    },
+    homeicon: {
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      backgroundColor: "#FFF",
+      alignItems: 'center',
+      justifyContent: 'center',
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+  },
 });
   export default Search;
