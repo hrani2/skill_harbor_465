003f159bd6961d6c57956b13d4de807fab266d08
ricliu <yuminml2@illinois.edu>
2023-11-28 19:14:25 -0600

    commit before pull

--- skill_harbor/ChooseTeam.js
+++ skill_harbor/ChooseTeam.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { queryTeamDb } from './firebase/utils';
 
 const ChooseTeam = ({navigation}) => {
     return (
