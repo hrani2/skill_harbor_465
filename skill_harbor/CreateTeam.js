325c456838ce194db4e99e73b906b358fc92927b
Dawn <dawnchen0909@gmail.com>
2023-12-01 15:09:46 -0600

    fix home press in createTeam

--- skill_harbor/CreateTeam.js
+++ skill_harbor/CreateTeam.js
@@ -51,13 +51,26 @@ const CreateTeam = ({ navigation, route }) => {
 
 
   const handleHomePress = () => {
-    try { 
-      navigation.navigate('Home', {email: email});
-    } catch (e) {
-      console.log("navigation error: ", e); 
-    }
+    // Show a confirmation dialog before navigating to the HomeScreen
+    Alert.alert(
+      'Confirmation',
+      'Are you sure you want to go to the home? Your progress will not be saved.',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: () => {
+            console.log('Home button pressed - confirmed');
+            navigation.navigate('Home', {email: email});
+          },
+        },
+      ],
+      { cancelable: false }
+    );
   };
-
 
   const handleAddSkill = () => {
     // Show the skill modal
